3f3694036a3e61b9be9b15fa9d8d9fd7801e3120
SergeyKutsko <kutsko86@gmail.com>
2022-07-16 23:05:46 +0300

    load to DB

--- scripts/login.py
+++ scripts/login.py
@@ -12,6 +12,40 @@ from selenium.webdriver.common.keys import Keys
 from datetime import date
 import os
 import time
+from app.models import PaymentsOrder
+import csv
+import datetime
+
+def save_report_to_db(file_name):
+    with open(file_name) as file:
+        reader = csv.reader(file)
+        next(reader)  # Advance past the header
+
+        for row in reader:
+            print(row)
+            film = PaymentsOrder(transaction_uuid = row[0],
+                                 driver_uuid = row[1],
+                                 drievr_name = row[2],
+                                 drievr_second_name = row[3],
+                                 trip_uuid = row[4],
+                                 trip_description = row[5],
+                                 organization_name = row[6],
+                                 organization_nickname = row[7],
+                                 transaction_time = datetime.datetime.strptime(row[8], "%Y-%m-%d %H:%M:%S.%f %z EEST"),
+                                 paid_to_you = row[9],
+                                 your_earnings = row[10],
+                                 cash = row[11],
+                                 fare = row[12],
+                                 tax = row[13],
+                                 fare2 = row[14],
+                                 service_tax = row[15],
+                                 wait_time = row[16],
+                                 out_of_city = row[17],
+                                 tips = row[18],
+                                 transfered_to_bank = row[19],
+                                 ajustment_payment =row[20],
+                                 cancel_payment = row[21])
+            film.save()
 
 def retry(fun, max_tries=2):
     for i in range(max_tries):
@@ -90,8 +124,12 @@ def generate_report(driver):
     driver.find_element_by_xpath('//div[@aria-roledescription="button"]/div[text()="1"]').click()
     end = driver.find_element_by_xpath('//div[@data-testid="end-date-picker"]/div/div/div/input')
     end.send_keys(Keys.NULL)
-    driver.find_element_by_xpath(f'//div[@aria-roledescription="button"]/div[text()="{date.today().strftime("%d")}"]').click()
+    today = date.today()
+    d, y, m  = today.strftime("%d"), today.strftime("%Y"), today.strftime("%m")
+    driver.find_element_by_xpath(f'//div[@aria-roledescription="button"]/div[text()="{d}"]').click()
     driver.find_element_by_xpath('//button[@data-testid="generate-report-button"]').click()
+
+    return f'{y}{m}01-{y}{m}{d}-payments_order-___.csv'
 
 def download_report(driver):
     b = driver.find_element_by_xpath('//div[1][@data-testid="payment-reporting-table-row"]/div/div/div/div/button')
@@ -100,10 +138,10 @@ def download_report(driver):
 
 def run():
     driver = retry(build_driver)
-    login(driver)
-    generate_report(driver)
+    #login(driver)
+    report_name = generate_report(driver)
     download_report(driver)
-
+    save_report_to_db(report_name)
     time.sleep(2000)
 
  
