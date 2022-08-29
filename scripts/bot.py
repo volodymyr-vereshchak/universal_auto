06abd06e792737ada588c7eee2e2053ee61dd269
SergeyKutsko <kutsko86@gmail.com>
2022-08-29 14:59:58 +0300

    cleanup

--- scripts/bot.py
+++ scripts/bot.py
@@ -13,8 +13,7 @@ PORT = int(os.environ.get('PORT', '8443'))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 TOKEN = os.environ['TELEGRAM_TOKEN']
 
@@ -23,10 +22,6 @@ TOKEN = os.environ['TELEGRAM_TOKEN']
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
 
 def echo(update, context):
     """Echo the user message."""
@@ -45,41 +40,17 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", report))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("report", report))
     dp.add_handler(CommandHandler("code", code))
-
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
-
     # log all errors
     dp.add_error_handler(error)
-
-    # Start the Bot
-    # updater.start_webhook(
-    #     listen="0.0.0.0",
-    #     port=int(PORT),
-    #     url_path=TOKEN,
-    #     webhook_url='https://snowy-wood-371.fly.dev/' + TOKEN
-    # )
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.start_polling()
     updater.idle()
-    logger.warning('APPAAA')
 
 def run():
     main()
