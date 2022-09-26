072f5ea90fdbc8a15344fa7d14459055e3fc2a6e
Serhii <panda.kef1rchik@gmail.com>
2022-09-27 00:25:57 +0300

    fix DB

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,5 +1,7 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import *
+from telegram.ext import *
+from requests import *
 import os
 import time
 import csv
@@ -20,12 +22,26 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    update.message.reply_text('Hi!')
+def start(update: Update, context: CallbackContext):
+    # print(update.effective_message.username)
+    # print(update.effective_message.text)
+    get_number_button = [[KeyboardButton("Надайте ваш номер телефону")]]
+    context.bot.send_message(chat_id=update.effective_chat_id, text='Hlo!',
+                             reply_markup=ReplyKeyboardMarkup(get_number_button))
+
+
+# def contact_callback(bot, update):
+#     contact = update.effective_message.contact
+#     phone = contact.phone_number
+#     print(contact)
+#     print(phone)
+#     update.message.reply_text('Thanks your data is accepted', start(), reply_keyboard=True)
+
 
 def report(update, context):
     update.message.reply_text("Enter you Uber OTP code from SMS:")
     update.message.reply_text(get_report())
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -39,11 +55,14 @@ def code(update, context):
     
 
 def main():
-    updater = Updater(os.environ['TELEGRAM_TOKEN'], use_context=True)
+    updater = Updater(os.environ["TELEGRAM_TOKEN"], use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("report", report, run_async=True))
+    # dp.add_handler(MessageHandler(Filters.contact,
+    #                               contact_callback, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.text, code))
+    # dp.add_handler(MessageHandler(Filters.contact, contact_callback))
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
