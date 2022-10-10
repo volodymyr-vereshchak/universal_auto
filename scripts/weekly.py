034fef7d33799a71961f9ee6286c08aa1f923dcf
Igor Sivak <isivak@mementia.com>
2022-10-11 00:20:42 +0300

    fix weekly file

--- scripts/weekly.py
+++ scripts/weekly.py
@@ -1,4 +1,4 @@
-from app.models import UberPaymentsOrder, BoltPaymentsOrder, UklonPaymentsOrder,
+from app.models import UberPaymentsOrder, BoltPaymentsOrder, UklonPaymentsOrder
 import sys
 import os
 
@@ -15,10 +15,10 @@ def run(*args):
 	BoltPaymentsOrder.download_bolt_weekly_file(files=files)
 
 	if args:
-	  week = f"2022W{args[0]}5"
+		week = f"2022W{args[0]}5"
 	else:
-	  week = None	
-	print(get_report(week_number = week, driver=False, sleep=0 , headless=True))
+		week = None
+	print(get_report(week_number=week, driver=False, sleep=0, headless=True))
 
-	
+
 
