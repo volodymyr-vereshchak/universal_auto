9f044de1fa5645069d7d7331cc511a789fb908a9
SergeyKutsko <kutsko86@gmail.com>
2022-11-22 08:44:35 +0200

    delete blog

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -5,6 +5,5 @@ from taxi_service.views import index, about, why, blog
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    path('blog/', blog, name='blog'),
     path('why/', why, name='why'),
 ]
